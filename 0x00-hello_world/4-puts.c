1c4cb10895c0ab522ab7179b156b63114262c2f5
Sibongile <sibongilemsiza29@gmail.com>
2023-07-13 04:17:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints a lime with puts function
+ * main - Entry point
  *
- * Return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
